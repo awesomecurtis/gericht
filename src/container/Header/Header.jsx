2d752408ef82ff96507c1d1d5cfef913506c7eea
awesomecurtis <88251469+awesomecurtis@users.noreply.github.com>
2023-10-19 10:48:32 +0000

    headMastr

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {SubHeading} from '../../components/';
+import { images } from '../../constants/';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className='app__header app__wrapper section__padding' id='home'>
+            <div className='app__header_info'>
+                <SubHeading title='Chase the new flavour'/>
+                <h1 className='app__header-h1'>The Key To Fine Dining</h1>
+                <p className='p__opensans' style={{margin: '2rem 0'}}>hChase the new flavoureader ChaChase the new flavourse the new flavourapp The Key Chase the new flavourTo Fine Dining</p>
+                <button type='button' className='custom__button'>Explore Menu</button>
+            </div>
+            <div className='app__header-img'>
+                <img src={images.welcome} alt='welcome' />
+            </div>
         </div>
     )
 }
