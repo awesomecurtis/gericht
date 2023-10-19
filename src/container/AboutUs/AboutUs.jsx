3ba1ed75e8b96b251b0171fdcfd873dde3fbfbed
awesomecurtis <88251469+awesomecurtis@users.noreply.github.com>
2023-10-19 12:25:05 +0000

    aboutHistory

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {images} from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => {
     return (
-        <div>
-            About Us
+        <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+            <div className='app__aboutus-overlay flex__center'>
+                <img src={images.G} alt='letter G'/>
+            </div>
+            <div className='app__aboutus-content flex__center'>
+                <div className='app__aboutus-content_about'>
+                    <h1 className='headtext__cormorant'>About us</h1>
+                    <img src={images.spoon} alt='about spoon' className='spoon__img' />
+                    <p className='p__opensans'>HChase The New Flavoureader ChaChase The New Flavourse The New Flavourapp The Key Chase The New FlavourTo Fine Dining</p>
+                    <button type='button' className='custom__button'>Know More</button>
+                </div>
+
+                <div className='app__aboutus-content_knife flex__center'>
+                    <img src={images.knife} alt='knife' />
+
+                </div>
+                <div className='app__aboutus-content_history'>
+                    <h1 className='headtext__cormorant'>Our History</h1>
+                    <img src={images.spoon} alt='about spoon' className='spoon__img' />
+                    <p className='p__opensans'>HChase The New Flavoureader ChaChase The New Flavourse The New Flavourapp The Key Chase The New FlavourTo Fine Dining</p>
+                    <button type='button' className='custom__button'>Know More</button>
+                </div>
+            </div>
         </div>
     )
 }
