b3869e838a3537f3ab2ab790d8e8a3cbc98b8be0
awesomecurtis <88251469+awesomecurtis@users.noreply.github.com>
2023-10-20 10:20:51 +0000

    resMenu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -5,7 +5,7 @@ import './SpecialMenu.css';
 
 const Menu = () => {
     return (
-        <div className='app__specialMenu flex__center'>
+        <div className='app__specialMenu flex__center section__padding' id='menu'>
             <div className='app_specialMenu-title'>
                 <SubHeading title='Menu that fits you palette' />
                 <h1 className='headtext__cormorant'>Today's special</h1>
@@ -22,7 +22,7 @@ const Menu = () => {
                 <div className='app__specialMenu-menu_img'>
                     <img src={ images.menu} alt='menu' />
                 </div>
-                <div className='app__specialMenu-menu_cocktail'>
+                <div className='app__specialMenu-menu_cocktail flex__center'>
                     <p className='app__specialMenu-menu_heading'>Cocktails</p>
                     <div className='app__specialMenu_menu_items'>
                         {data.cocktails.map((cocktail, index) => (
